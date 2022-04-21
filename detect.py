1e5be443c5eb5fea04bef8f11cad1a0b7d4850c2
Chandra Prakash Singh <chandra.ps612@gmail.com>
2022-04-21 20:57:24 +0530

    modified detect.py and added post_processed image

--- detect.py
+++ detect.py
@@ -2,21 +2,27 @@ import cv2
 import numpy as np
 import argparse
 
-'''This following object detection code works smoothly with yolov3, and yolov4.'''
+'''This following object detection code works smoothly with yolo darknet model.'''
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--webcam', help='True/False', default=False)
-parser.add_argument('--play_video', help='True/False', default=False)
+parser.add_argument('--webcam', help='True/False', default=True)
+parser.add_argument('--video_feed', help='True/False', default=False)
 parser.add_argument('--image', help='True/False', default=False)
-parser.add_argument('--video_path', help='Path of video file', default='Path of video file')
-parser.add_argument('--image_path', help='Path of image file', default='Path of image file')
+parser.add_argument('--video_path', help='Path of video file', default='C:\\Users\\lav singh\\workplace\\yolo\\test.mp4')
+parser.add_argument('--image_path', help='Path of image file', default='C:\\Users\\lav singh\\workplace\\yolo\\test.jpg')
 parser.add_argument('--verbose', help='To print statements', default=True)
 args = parser.parse_args()
 
-configPath='Path of config file'
-weightsPath='Path of weights file'
-imagePath='Path of image file'
-videoPath='Path of video file'
+configPath='C:\\Users\\lav singh\\workplace\\yolo\\yolov4-tiny.cfg'
+weightsPath='C:\\Users\\lav singh\\workplace\\yolo\\yolov4-tiny.weights'
+imagePath='C:\\Users\\lav singh\\workplace\\yolo\\test.jpg'
+videoPath='C:\\Users\\lav singh\\workplace\\yolo\\test.mp4'
+outputImagePath='C:\\Users\\lav singh\\workplace\\yolo\\imgr.jpg'
+
+# Constants:
+CONF_THRESHOLD=0.45
+NMS_THRESHOLD=0.45
+SCORE_THRESHOLD=0.5
 
 # Load yolo pre-trained model
 def load_yolo():
@@ -34,7 +40,7 @@ def load_yolo():
 
 def load_image(imagePath):
     img=cv2.imread(imagePath) # Loading image
-    imgr=cv2.resize(img, (640, 640), fx=None, fy=None)
+    imgr=cv2.resize(img, (416, 416), fx=None, fy=None)
     height, width,_= imgr.shape
     return imgr, height, width
 
@@ -48,7 +54,7 @@ def start_video(videoPath): #Triggering video source for object detection
 
 def detect_objects(imgr, net, output_layers):
     # Using blob function of openCV to pre-process image
-    blob=cv2.dnn.blobFromImage(imgr, 1/255, (416, 416), (0,0,0), True, False) 
+    blob=cv2.dnn.blobFromImage(imgr, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False) 
     net.setInput(blob)
     outputs=net.forward(output_layers)
     return blob, outputs
@@ -59,26 +65,25 @@ def get_box_dimensions(outputs, width, height): # Showing information on the scr
     boxes=[]
     for output in outputs:
         for detection in output:
-            scores= detection[5:]
-            class_id=np.argmax(scores)
-            conf= scores[class_id]
-            if conf>0.5:
-                 # Object detected
-                 center_x=int(detection[0]*width)
-                 center_y=int(detection[1]*height)
-                 w=int(detection[2]*width)
-                 h=int(detection[3]*height)
-                 # Rectangle co-ordinates
-                 x=int(center_x-w/2)
-                 y=int(center_y-h/2)
-                 boxes.append([x, y, w, h])
-                 confs.append(float(conf))
-                 class_ids.append(class_id)
+            classes_scores= detection[5:]
+            class_id=np.argmax(classes_scores)
+            conf= detection[4]
+            if conf>=CONF_THRESHOLD:
+                center_x=int(detection[0]*width)
+                center_y=int(detection[1]*height)
+                w=int(detection[2]*width)
+                h=int(detection[3]*height)
+                # Rectangle co-ordinates
+                x=int(center_x-w/2)
+                y=int(center_y-h/2)
+                boxes.append([x, y, w, h])
+                confs.append(float(conf))
+                class_ids.append(class_id)
     return class_ids, confs, boxes
 
 def draw_labels(boxes, confs, class_ids, classes, imgr):
     # Using NMS function of openCV to perform non-maximum suppression 
-    indices=cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.3)
+    indices=cv2.dnn.NMSBoxes(boxes, confs, SCORE_THRESHOLD, NMS_THRESHOLD)
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
     for i in range (len(boxes)):
         if i in indices:
@@ -98,6 +103,7 @@ def image_detection(imagePath):
     blob, outputs= detect_objects(imgr, net, output_layers)
     class_ids, confidences, boxes= get_box_dimensions(outputs, width, height)
     draw_labels(boxes, confidences, class_ids, classes, imgr)
+    cv2.imwrite(outputImagePath, imgr)
     while True:
         key=cv2.waitKey(1)
         if key==ord('q'):
@@ -108,6 +114,7 @@ def webcam_detection():
     cap= start_webcam()
     while True:
         _, frame=cap.read()
+        frame=cv2.resize(frame, (416, 416), fx=None, fy=None)
         height, width,_= frame.shape
         blob, outputs= detect_objects(frame, net, output_layers)
         class_ids, confidences, boxes= get_box_dimensions(outputs, height, width)
@@ -122,31 +129,37 @@ def video_detection(videoPath):
     cap= start_video(videoPath)
     while True:
         _, frame=cap.read()
+        frame=cv2.resize(frame, (416, 416), fx=None, fy=None)
         height, width,_= frame.shape
         blob, outputs= detect_objects(frame, net, output_layers)
         class_ids, confidences, boxes= get_box_dimensions(outputs, height, width)
         draw_labels(boxes, confidences, class_ids, classes, frame)
+        if create is None:
+            fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+            create = cv2.VideoWriter(output_videoPath, fourcc, 10, (1920, 1080), True)
+        create.write(frame)
         key=cv2.waitKey(1) # It'll generate a new frame after every 1 ms.
         if key==ord('q'):
             break
     cap.release()
+    create.release()
 
 if __name__ == '__main__':
 	webcam = args.webcam
-	video_play = args.play_video
+	video_feed = args.video_feed
 	image = args.image
 	if webcam:
 		if args.verbose:
-			print('Opening webcam...')
+			print('---- Starting Webcam object detection ----')
 		webcam_detection()
-	if video_play:
+	if video_feed:
 		videoPath = args.video_path
 		if args.verbose:
-			print('Opening '+videoPath+'...')
+			print('Opening '+videoPath+" .... ")
 		start_video(videoPath)
 	if image:
 		imagePath = args.image_path
 		if args.verbose:
-			print('Opening '+imagePath+'...')
+			print("Opening "+imagePath+" .... ")
 		image_detection(imagePath)
 	cv2.destroyAllWindows()
